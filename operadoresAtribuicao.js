3cdc3549218ac129ef7b90c42e418a43ccc53e32
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 23:13:55 -0300

    Aula 09 / Operadaores de Atribuição

---
+++ operadoresAtribuicao.js
@@ -0,0 +1,10 @@
+// Operadores de Atribuição
+
+let valorTeclado = 100
+    console.log(valorTeclado)
+
+valorTeclado += valorTeclado
+    console.log(valorTeclado)
+
+valorTeclado -= 50
+    console.log(valorTeclado)
