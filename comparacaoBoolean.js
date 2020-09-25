baa5248fa8318b9ae01914d059b9c57202812c7c
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-25 13:11:48 -0300

    Aula 013 / Comparação de Booleanos

---
+++ comparacaoBoolean.js
@@ -0,0 +1,15 @@
+// Falsy
+// Undefined 
+// Null
+// 0
+// False
+// ''
+// NaN - not a number
+
+// Truthy
+
+let corPersonalizada = ""
+let corPadrao = "Branco"
+let corPerfil = corPersonalizada || corPadrao
+
+    console.log(corPerfil)
