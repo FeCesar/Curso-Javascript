b5b4b5864c2506b1973d2d2df14ec25af98a28a9
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 22:37:27 -0300

    Aula 04 / Objetos

---
+++ objetos.js
@@ -0,0 +1,17 @@
+let pessoa = {
+    nome: "Felipe",
+    sobrenome: "Cesar",
+    idade: 17,
+    estaAprovado: true
+}
+
+let carros = {
+    marca: "Fiat",
+    placa: "ABB-9876",
+    dono: "José de Pedro",
+    documentoPago: true,
+    valor: 14500
+}
+
+console.log(pessoa)
+console.log(carros)
