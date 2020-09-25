fc138134368278acb52344867dc5fc1cfbf6059f
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 23:33:26 -0300

    Aula 012 / Operadores Lógicos

---
+++ operadoresLogicos.js
@@ -0,0 +1,19 @@
+// Operadores Lógicos
+// AND(&&)
+
+    // let maiorIdade = true
+    // let carteiraTrabalho = true
+    // let apto = maiorIdade && carteiraTrabalho
+    //     console.log(apto)
+
+// OR (||)
+
+    let menorIdade = false
+    let possuiCnh = false
+    let estaApto = menorIdade || possuiCnh
+        console.log("O canditado está apto para Participar? " + estaApto)
+
+// NOT (!)
+
+    let canditadoRecusado = !estaApto
+        console.log("O canditado foi recusado? " + canditadoRecusado)
