29264aaf2690648512e24c84fffd199144527c25
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 23:10:49 -0300

    Aula 08 / Operadores Aritiméticos

---
+++ operadoresAritimeticos.js
@@ -0,0 +1,12 @@
+// Operadores Aritiméticos
+    let salario = 100
+
+    console.log(salario + salario)
+    console.log(salario - salario)
+    console.log(salario * salario)
+    console.log(salario / salario)
+    console.log(salario ** salario)
+
+    let idade = 18
+    console.log(++idade)
+    console.log(--idade)
