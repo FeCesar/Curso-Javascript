1dcd314f24726e087ac19ba97bba263d1cb6d969
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 22:57:25 -0300

    Aula 06 / Functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+// Função = Verbo + Substantivo
+
+// let corSite = "Azul"
+//     function resetaCor(cor, tonalidade){
+//         corSite = cor + " " + tonalidade
+
+//     }
+// console.log(corSite)
+// resetaCor("Vermelho", "Escuro")
+// console.log(corSite)
+
+let valorAleatorio = "Oi"
+
+function adicionaValor(valor){
+    valorAleatorio += " " + valor
+}
+
+adicionaValor("Felipe")
+console.log(valorAleatorio)
