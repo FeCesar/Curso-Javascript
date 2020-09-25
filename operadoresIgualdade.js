ac009f8191c3bff7d078609745775b3d63795e01
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 23:18:01 -0300

    Aula 010 / Operadores de Igualdade

---
+++ operadoresIgualdade.js
@@ -0,0 +1,8 @@
+// Operadores de Igualdade
+// Igualdade Estrita (comparação de tipos)
+    console.log (1 === 1) // true
+    console.log ('1' === 1) // false
+
+// Igualdade Solta
+    console.log (1 == 1) // true
+    console.log ('1' == 1) // true
