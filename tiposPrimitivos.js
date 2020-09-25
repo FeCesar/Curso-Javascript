f420e09048b3632457d3e27a827bc04921d4b3a3
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 22:24:03 -0300

    Aula 03 / Tipos Primitivos

---
+++ tiposPrimitivos.js
@@ -0,0 +1,8 @@
+// Tipos de referencias
+
+let nome = "Felipe" //String
+let idade = 17 //Number
+let altura = 1.80 //Float
+let status = false //Boolean
+let sobrenome //Undefined
+let corSelecionada = null //Null
