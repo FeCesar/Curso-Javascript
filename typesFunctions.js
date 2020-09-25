1d37d7d6598a519afa05ee5c6c283d55af3d3062
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 23:02:49 -0300

    Aula 07 / Type Functions

---
+++ typesFunctions.js
@@ -0,0 +1,11 @@
+// Função Realiza uma Tarefa sem return
+function dizerNome(){
+    console.log("Felipe")
+}
+dizerNome()
+
+// Função que retorna com return
+function quadradoValor(valor){
+    return valor * valor
+}
+console.log(quadradoValor(12))
