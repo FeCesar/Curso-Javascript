e82c5f492d79d51d602fcb44cffbbb79d4913500
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-25 13:19:59 -0300

    MiniProjetos / Troca de Valores

---
+++ Mini-projetos/projeto1.js
@@ -0,0 +1,12 @@
+// Troca de valores
+let a = "Azul"
+let b = "Branco"
+
+function trocaValores(a ,b){
+    valorA = b
+    valorB = a
+    console.log(valorA)
+    console.log(valorB)
+}
+
+trocaValores(a, b)
