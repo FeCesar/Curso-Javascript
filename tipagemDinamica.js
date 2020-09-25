6584f853f84526cd93c58379be87408bceeca414
Felipe Cesar <ff.cc.ss.rr@hotmail.com>
2020-09-24 22:30:10 -0300

    Aula 04 / Tipagem Dinâmica

---
+++ tipagemDinamica.js
@@ -0,0 +1,11 @@
+let nome = "Felipe"
+let idade = 21
+let estaAprovado = false
+let sobrenome
+let cor = null
+
+console.log(typeof(nome))
+console.log(typeof(idade))
+console.log(typeof(estaAprovado))
+console.log(typeof(sobrenome))
+console.log(typeof(cor))
